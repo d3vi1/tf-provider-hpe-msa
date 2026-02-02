d368a8e664c20b490e00477f9046ad0bf917b79a
Răzvan Corneliu C.R. VILT <16434603+d3vi1@users.noreply.github.com.>
2026-02-02 11:23:35 +0200

    Validate host name length

--- internal/provider/resource_host_initiator.go
+++ internal/provider/resource_host_initiator.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
@@ -45,6 +46,9 @@ func (r *hostInitiatorResource) Schema(_ context.Context, _ resource.SchemaReque
 			"host_name": schema.StringAttribute{
 				Description: "Host name.",
 				Required:    true,
+				Validators: []validator.String{
+					hostNameValidator{},
+				},
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.RequiresReplace(),
 				},
